d684ca0cfdaff1d88aaeb063082c010e5595e01f
Neha <neha@gmail.com>
2023-01-07 12:15:32 -0330

    my files 3

--- task2_copy_all_files_other_folder.py
+++ task2_copy_all_files_other_folder.py
@@ -1,8 +1,30 @@
 import os
 import shutil
 
-os.chdir("D:\ALL FILES")
-shutil.copytree('IMP_FILES', 'WORDF_PDF')
+# os.chdir("D:\ALL FILES")
+# shutil.copytree('IMP_FILES', 'WORDF_PDF')
+print("Avinash.COM SH001")
+print("Avinash.COM SH002")
 
+print("Avinash.COM SH003")
 
-print("HQ VFX")
+print("Avinash.COM SH003")
+
+print("Avinash.COM SH003")
+print("Avinash.COM SH003")
+
+print("Avinash.COM SH003")
+print("Avinash.COM SH003")
+print("Avinash.COM SH003")
+
+print("HQVFX.COM SH003")
+
+print("Avinash.COM SH003")
+print("Avinash.COM SH003")
+print("Avinash.COM SH003")
+print("Avinash.COM SH003")
+print("Avinash.COM SH003")
+print("Avinash.COM SH003")
+print("Avinash.COM SH003")
+print("Avinash.COM SH003")
+print("Avinash.COM SH003")
